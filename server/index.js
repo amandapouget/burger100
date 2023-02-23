0907ecd6faf74194a4aa2542ee7959113d730df7
Amanda Pouget <amanda.pouget@gmail.com>
2023-02-22 21:44:07 -0500

    Shamefully commit my Yelp API key directly to the codebase (in lieu of git-secret, tokenization, etc)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 8000;
