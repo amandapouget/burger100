408c813f7ce6e9b5270ba0e635de8df0d297718d
Amanda Pouget <amanda.pouget@gmail.com>
2023-02-27 23:31:17 -0500

    Make the Yelp api url a constant

--- server/yelp.js
+++ server/yelp.js
@@ -6,9 +6,10 @@ import fetch from 'node-fetch';
 // That way, I don't have to grant access to each interviewer/reviewer who might run this locally.
 const YELP_API_KEY = "lccMWPKR6cqq8gBhbIQen8ooMu4YHk8ky-45m-U_CCjDwWMphtat1jikJqO9Lb8KS_D9SMmwOscs7_9TG6u5aSv-D7Rqy2jIpO-gikvtqHtrVXPq3W4NX2y0bM72Y3Yx"
 const YELP_MAX_RESULTS_PER_REQUEST = 50;
+const YELP_API = 'https://api.yelp.com/v3/graphql';
 
 const fetchBurgerJoints = async ({ location, price, offset }) => {
-  const yelpApiResponse = await fetch('https://api.yelp.com/v3/graphql', {
+  const yelpApiResponse = await fetch(YELP_API, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/graphql',
